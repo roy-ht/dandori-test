0dfe6d1703dd7fe8754319c2061dc386fc9eba98
Hiroyuki Tanaka <aflc0x@gmail.com>
2021-05-27 13:07:57 +0900

    Add sample

---
+++ module_handler.py
@@ -0,0 +1,5 @@
+def handle_issue_comment(ctx):
+    """handle issue_comment event, some slash command"""
+    body = ctx.gh.comment_body()
+    if body.startswith("/greet"):
+        print("Greet command invoked from dandor-test repo (module)!!!")
